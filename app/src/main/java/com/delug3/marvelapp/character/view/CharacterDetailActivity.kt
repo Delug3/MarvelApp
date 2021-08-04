6ff1e2ec48b659f8f8304e0b9dc0afd111d7ce2a
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2021-08-04 12:56:31 +0200

    Implemented dynamic view with comic information. Added nullability comprobation. Fixed image not showing if it is comig from unsecure url

--- app/src/main/java/com/delug3/marvelapp/character/view/CharacterDetailActivity.kt
+++ app/src/main/java/com/delug3/marvelapp/character/view/CharacterDetailActivity.kt
@@ -2,9 +2,12 @@ package com.delug3.marvelapp.character.view
 
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.delug3.marvelapp.R
+import com.delug3.marvelapp.character.model.ItemsItem
 import com.delug3.marvelapp.character.model.Thumbnail
 import com.delug3.marvelapp.character.viewmodels.CharacterDetailViewModel
 import com.delug3.marvelapp.common.utilities.Constants
@@ -14,14 +17,16 @@ import com.squareup.picasso.Picasso
 class CharacterDetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityDetailCharactersBinding
     private val characterDetailViewModel: CharacterDetailViewModel by viewModels()
-    private var characterId: Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailCharactersBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var characterId = 0
         if (savedInstanceState == null) {
+
             val extras = intent.extras
             if (extras != null) {
                 characterId = extras.getInt(Constants.CHARACTER_ID)
@@ -29,30 +34,69 @@ class CharacterDetailActivity : AppCompatActivity() {
         }
         setCharacterId(characterId)
         getData()
-        updateUI()
+
+        initUI()
     }
 
     private fun getData() {
         characterDetailViewModel.getCharacterDetails()
     }
 
-    private fun updateUI() {
+    private fun initUI() {
+        setCharacterName()
+        setCharacterDescription()
+        setCharacterImage()
+        setComicsAppearance()
+    }
 
+    private fun setComicsAppearance() {
+        val comics = Observer<List<ItemsItem>?> { comics ->
+            setComicsAppearanceDynamically(comics)
+        }
+        characterDetailViewModel.characterAppearanceInComics.observe(this, comics)
+    }
+
+    private fun setCharacterImage() {
+        val characterThumbnailObserver = Observer<Thumbnail> { characterThumbnail ->
+            val thumbnail = "${characterThumbnail.path}.${characterThumbnail.extension}"
+            val secureThumbnail: String = thumbnail.replace("http", "https")
+            Picasso.get().load(secureThumbnail).error(R.drawable.error).into(binding.ivCharacter)
+        }
+        characterDetailViewModel.characterThumbnail.observe(this, characterThumbnailObserver)
+    }
+
+    private fun setCharacterDescription() {
+        val characterDescriptionObserver = Observer<String> { characterDescription ->
+            if (characterDescription == "") {
+                binding.tvDescription.text = Constants.NO_DESCRIPTION
+            } else {
+                binding.tvDescription.text = characterDescription
+            }
+        }
+        characterDetailViewModel.characterDescription?.observe(this, characterDescriptionObserver)
+    }
+
+    private fun setCharacterName() {
         val characterNameObserver = Observer<String> { characterName ->
             binding.tvName.text = characterName
         }
-        val characterDescriptionObserver = Observer<String> { characterDescription ->
-            binding.tvDescription.text = characterDescription
-        }
-
-        val characterThumbnailObserver = Observer<Thumbnail> { characterThumbnail ->
-            val thumbnail = "${characterThumbnail.path}.${characterThumbnail.extension}"
-            Picasso.get().load(thumbnail).into(binding.ivCharacter)
-        }
-
         characterDetailViewModel.characterName?.observe(this, characterNameObserver)
-        characterDetailViewModel.characterDescription?.observe(this, characterDescriptionObserver)
-        characterDetailViewModel.characterThumbnail.observe(this, characterThumbnailObserver)
+    }
+
+    private fun setComicsAppearanceDynamically(comics: List<ItemsItem>?) {
+        if (comics?.isEmpty() == true) {
+            val tvDynamic = TextView(this)
+            tvDynamic.textSize = 15f
+            tvDynamic.text = Constants.NO_COMICS
+            binding.llComicsLayout.addView(tvDynamic)
+        } else {
+            for (i in comics?.indices!!) {
+                val tvDynamic = TextView(this)
+                tvDynamic.textSize = 15f
+                tvDynamic.text = comics[i].name
+                binding.llComicsLayout.addView(tvDynamic)
+            }
+        }
     }
 
     private fun setCharacterId(characterId: Int) {
