ad035692db88a277797218a6b117981f84e79b5f
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2021-08-04 12:58:45 +0200

    Added new constants fields

--- app/src/main/java/com/delug3/marvelapp/common/utilities/Constants.kt
+++ app/src/main/java/com/delug3/marvelapp/common/utilities/Constants.kt
@@ -6,13 +6,18 @@ import java.security.MessageDigest
 class Constants {
 
     companion object {
+
+
         val timeStamp = System.currentTimeMillis().toString()
 
         const val BASE_URL = "https://gateway.marvel.com/"
         const val PUBLIC_KEY = "3bdb4c120b096da4fc94c7c938a9b33f"
         const val PRIVATE_KEY = "ac93aa83c707d957155ab6a1c2c7ca13f63e9b4f"
         const val CHARACTER_ID = "CHARACTER_ID"
-
+        const val NO_CONNECTION = "No internet connection"
+        const val NO_DESCRIPTION =
+            "Sorry, but there is no description available for this character yet"
+        const val NO_COMICS = "No appearances for this character"
 
         fun hashKey(): String {
             val input = timeStamp + PRIVATE_KEY + PUBLIC_KEY
